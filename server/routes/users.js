37fa5cc8327a9986d891e7dbff805c314b493848
Tom McTiernan <thomasmctiernan@gmail.com>
2017-01-07 14:48:38 +0000

    Save users in memory

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var users = ['admin'];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(JSON.stringify(users));
+});
+
+/* POST a new user. */
+router.post('/', function(req, res, next) {
+  var user = req.body.name;
+  if (!user) {
+    throw new Error('name please');
+  } else {
+    users.push(user);
+    res.send(JSON.stringify(users));
+  }
 });
 
 module.exports = router;
